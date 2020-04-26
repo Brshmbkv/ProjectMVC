fcf6c63c1b0ce025f6de779de71767ae3a7822ef
Bekzhan Bershimbekov <Bekzhan99@gmail.com>
2020-04-26 21:44:34 +0600

    Добавьте файлы проекта.

---
+++ Project/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; } 
+        public string LongDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public bool IsCourseOfTheWeek { get; set; }
+        public bool InStock { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ Project/Models/MockCourseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class MockCourseRepository : ICourseRepository
+    {
+        private readonly ICategoryRepository _categoryRepository = new MockCategoryRepository();
+
+        public IEnumerable<Course> AllCourses =>
+            new List<Course>
+            {
+                new Course{CourseId = 1, Name = "The Complete JavaScript Course 2020: Build Real Projects!", Price = 20M,ShortDescription="s desc", LongDescription = "l desc", ImageUrl="url",ImageThumbnailUrl="url",IsCourseOfTheWeek = true,InStock = true, Category = _categoryRepository.AllCategories.ToList()[0]},
+                new Course{CourseId = 2, Name = "Modern JavaScript From The Beginning", Price = 39M,ShortDescription="s desc", LongDescription = "l desc", ImageUrl="url",ImageThumbnailUrl="url",IsCourseOfTheWeek = true,InStock = true, Category = _categoryRepository.AllCategories.ToList()[0]},
+                new Course{CourseId = 3, Name = "The Complete Android N Developer Course", Price = 19M, ShortDescription="s desc", LongDescription = "l desc", ImageUrl="url",ImageThumbnailUrl="url",IsCourseOfTheWeek = true,InStock = true, Category = _categoryRepository.AllCategories.ToList()[1]},
+
+            };
+    }
+}
